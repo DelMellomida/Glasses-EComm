cf61a6e302c21b07bcbe3c64f7d6795638830618
J-Ocariza <153717036+J-Ocariza@users.noreply.github.com>
2025-04-21 20:25:22 +0800

    database Migrate

---
+++ database/migrations/2025_04_21_121126_product_details.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id('Product_ID')->primary();
+            $table->string('Product_Name')->nullable();
+            $table->string('Product_Description')->nullable();
+            $table->integer('Price')->default(0);
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
---
+++ database/migrations/2025_04_21_121141_product_images.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->integer('Product_ID');
+            $table->string('Image_Path')->nullable();
+            $table->timestamps();
+        
+            $table->foreign('Product_ID')->references('Product_ID')->on('product_details')->onDelete('cascade');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_images');
+    }
+};
